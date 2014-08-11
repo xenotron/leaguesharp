6cbaad518b1261f8c50d2ebb2e87ebd320fee3d8
trees-software <treesbot@gmail.com>
2014-08-11 14:53:10 -0400

    Update AutoLantern.cs

--- AutoLantern.cs
+++ AutoLantern.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using LeagueSharp;
 using LeagueSharp.Common;
@@ -20,23 +19,19 @@ namespace AutoLantern
 
         static void Main(string[] args)
         {
-            Game.OnGameStart += OnGameStart;
             Game.OnGameUpdate += OnGameUpdate;
             Obj_AI_Minion.OnCreate += OnMinionCreation;
             Obj_AI_Minion.OnDelete += OnMinionDeletion;
-        }
-
-        static void OnGameStart(EventArgs args)
-        {
 
             Menu = new Menu("AutoLantern", "AutoLantern", true);
             Menu.AddItem(new MenuItem("Auto", "Auto-Lantern at Low HP").SetValue(true));
             Menu.AddItem(new MenuItem("Low", "Low HP Percent").SetValue(new Slider(20, 30, 5)));
             Menu.AddItem(new MenuItem("Hotkey", "Hotkey").SetValue(new KeyBind(32, KeyBindType.Press, false)));
             Menu.AddToMainMenu();
-      
+
             Game.PrintChat("AutoLantern by Trees loaded.");
             Player = ObjectManager.Player;
+
         }
 
         private static void OnGameUpdate(EventArgs args)
