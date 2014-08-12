ac35f86c59121aa47dc1a1462c2545e9d90f2bac
trees-software <treesbot@gmail.com>
2014-08-12 18:41:22 -0400

    Update AutoIgnite.cs

--- AutoIgnite.cs
+++ AutoIgnite.cs
@@ -1,4 +1,5 @@
-#region Includes
+#region Imports
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using LeagueSharp;
 using SharpDX;
+
 #endregion
 
 namespace AutoIgnite
