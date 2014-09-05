0aebb52eb190c51dab1a0dacd7bbc06d8718e5f5
Trees <treesbot@gmail.com>
2014-09-05 08:44:30 -0400

    Update
    Added harass mode, added e hitchance. Add ignite ks.

--- TUrgot/Program.cs
+++ TUrgot/Program.cs
@@ -19,7 +19,7 @@ namespace TUrgot
 
         public static List<Spell> SpellList = new List<Spell>();
         public static Spell Q, Q2, W, E;
-
+        public static SpellDataInst Ignite;
         public static Menu Menu;
 
         private static void Main(string[] args)
@@ -45,6 +45,9 @@ namespace TUrgot
             SpellList.Add(W);
             SpellList.Add(E);
 
+
+            Ignite = Player.Spellbook.GetSpell(Player.GetSpellSlot("summonerdot"));
+
             Menu = new Menu("Trees " + ChampName, ChampName, true);
 
             Menu.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
@@ -55,10 +58,18 @@ namespace TUrgot
             Menu.AddSubMenu(ts);
 
             Menu.AddSubMenu(new Menu("Combo", "Combo"));
-            Menu.SubMenu("Combo").AddItem(new MenuItem("Q", "Use Q").SetValue(true));
-            Menu.SubMenu("Combo").AddItem(new MenuItem("E", "Use E").SetValue(true));
+            Menu.SubMenu("Combo").AddItem(new MenuItem("ComboQ", "Use Q").SetValue(true));
+            Menu.SubMenu("Combo").AddItem(new MenuItem("ComboE", "Use E").SetValue(true));
+            Menu.SubMenu("Combo").AddItem(new MenuItem("ComboEChance", "E HitChance").SetValue(new Slider(2, 1, 3)));
             Menu.SubMenu("Combo")
                 .AddItem(new MenuItem("ComboActive", "Combo").SetValue(new KeyBind(32, KeyBindType.Press)));
+
+            Menu.AddSubMenu(new Menu("Harass", "Harass"));
+            Menu.SubMenu("Harass").AddItem(new MenuItem("HarassQ", "Use Q").SetValue(true));
+            Menu.SubMenu("Harass").AddItem(new MenuItem("HarassE", "Use E").SetValue(true));
+            Menu.SubMenu("Harass").AddItem(new MenuItem("HarassEChance", "E HitChance").SetValue(new Slider(3, 1, 3)));
+            Menu.SubMenu("Harass")
+                .AddItem(new MenuItem("HarassActive", "Harass").SetValue(new KeyBind((byte)'C', KeyBindType.Press)));
 
             Menu.AddSubMenu(new Menu("Drawings", "Drawings"));
             Menu.SubMenu("Drawings")
@@ -91,14 +102,18 @@ namespace TUrgot
 
         private static void CastLogic()
         {
-            //KSLogic();
+            KSLogic();
             SmartQ();
 
             var target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Physical);
-            if (target == null || !Menu.Item("ComboActive").GetValue<KeyBind>().Active) return;
+            if (target == null ||
+                (!Menu.Item("ComboActive").GetValue<KeyBind>().Active &&
+                 !Menu.Item("HarassActive").GetValue<KeyBind>().Active)) return;
 
-            CastE(target);
-            CastQ(target);
+            var mode = Menu.Item("ComboActive").GetValue<KeyBind>().Active ? "Combo" : "Harass";
+
+            CastE(target, mode);
+            CastQ(target, mode);
         }
 
         private static void SmartQ()
@@ -119,41 +134,62 @@ namespace TUrgot
             }
         }
 
-        private static void CastQ(Obj_AI_Base target)
+        private static void CastQ(Obj_AI_Base target, string mode)
         {
-            if (Q.IsReady() && Menu.Item("Q").GetValue<bool>() &&
+            if (Q.IsReady() && Menu.Item(mode + "Q").GetValue<bool>() &&
                 Player.Distance(target) < Q.Range)
                 Q.Cast(target);
         }
 
-        private static void CastE(Obj_AI_Base target)
+        private static void CastE(Obj_AI_Base target, string mode)
         {
-            if (!E.IsReady() || !Menu.Item("E").GetValue<bool>())
+            if (!E.IsReady() || !Menu.Item(mode + "E").GetValue<bool>())
                 return;
 
+            var hitchance = GetHitchance(Menu.Item(mode + "EChance").GetValue<int>());
+
             if (Player.ServerPosition.Distance(target.ServerPosition) < E.Range)
-                E.Cast(target);
+                E.CastIfHitchanceEquals(target, hitchance);
             else
                 E.CastIfHitchanceEquals(SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical), HitChance.High);
         }
 
-        /*private static void KSLogic()
-{
+        private static void KSLogic()
+        {
+            if (Ignite != null && Ignite.Slot != SpellSlot.Unknown && Ignite.State == SpellState.Ready &&
+                Player.CanCast)
+                KSIgnite();
+        }
 
-}
 
-private static void KSIgnite()
-{
-    var dmg = 50 + 20 * Player.Level;
-    foreach (
-        var obj in
-            ObjectManager.Get<Obj_AI_Hero>()
-                .Where(obj => obj.IsValid && obj.IsEnemy && obj.IsValidTarget(600, true, Player.ServerPosition) &&
-            obj.Health < dmg))
-    {
+        private static void KSIgnite()
+        {
+            var dmg = 50 + 20 * Player.Level;
+            foreach (
+                var obj in
+                    ObjectManager.Get<Obj_AI_Hero>()
+                        .Where(
+                            obj => obj.IsValid && obj.IsEnemy && obj.IsValidTarget(600, true, Player.ServerPosition) &&
+                                   obj.Health < dmg))
+            {
+                Player.SummonerSpellbook.CastSpell(Ignite.Slot, obj);
+            }
+        }
 
-    }
-}*/
 
+        private static HitChance GetHitchance(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    return HitChance.Low;
+                case 2:
+                    return HitChance.Medium;
+                case 3:
+                    return HitChance.High;
+                default:
+                    return HitChance.High;
+            }
+        }
     }
 }
