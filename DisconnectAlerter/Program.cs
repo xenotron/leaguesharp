d3e4209815c84bc80cda4c87ab68d07892daffb5
Trees <treesbot@gmail.com>
2014-09-22 05:29:20 -0400

    DisconnectAlerter

---
+++ DisconnectAlerter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace DisconnectAlerter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Game.OnGameProcessPacket += Game_OnGameProcessPacket;
+        }
+
+        private static void Game_OnGameProcessPacket(GamePacketEventArgs args)
+        {
+            if (args.PacketData[0] == Packet.S2C.PlayerDisconnect.Header)
+            {
+
+                Game.PrintChat(
+                    "<b><font color=\"#FF0000\">" +
+                    Packet.S2C.PlayerDisconnect.Decoded(args.PacketData).Player.ChampionName +
+                    "</font></b><font color=\"#FFFFFF\" has disconnected!</font></b>");
+            }
+        }
+    }
+}
