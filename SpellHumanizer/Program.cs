a7eeeca119c425f82cd83cd1ea9c647d7e527b89
Trees <treesbot@gmail.com>
2014-10-12 14:41:53 -0400

    SpellHumanizer
    Use with TRUS Humanizer for maximum humanization.

---
+++ SpellHumanizer/Program.cs
@@ -0,0 +1,40 @@
+﻿#region
+
+using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace SpellHumanizer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Game.OnGameSendPacket += Game_OnGameSendPacket;
+        }
+
+        private static void Game_OnGameSendPacket(GamePacketEventArgs args)
+        {
+            if (args.PacketData[0] != Packet.C2S.Cast.Header)
+            {
+                return;
+            }
+
+            var spellState = ObjectManager.Player.Spellbook.CanUseSpell((SpellSlot) args.PacketData[6]);
+
+            if (ObjectManager.Player.IsDead || spellState == SpellState.Cooldown || spellState == SpellState.NoMana ||
+                spellState == SpellState.NotLearned || spellState == SpellState.Surpressed ||
+                spellState == SpellState.Unknown)
+            {
+                args.Process = false;
+            }
+        }
+    }
+}
