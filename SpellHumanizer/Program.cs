83ee143af0c4d8ca66fa5b26d0d21d4c34cec69c
Trees <treesbot@gmail.com>
2014-10-18 20:32:18 -0400

    Added Debug

--- SpellHumanizer/Program.cs
+++ SpellHumanizer/Program.cs
@@ -10,6 +10,8 @@ namespace SpellHumanizer
 {
     internal class Program
     {
+        public static Menu Menu;
+
         public static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
@@ -17,7 +19,16 @@ namespace SpellHumanizer
 
         private static void Game_OnGameLoad(EventArgs args)
         {
-            LeagueSharp.Common.SpellHumanizer.Enabled = true;
+            Menu = new Menu("SpellHumanizer", "SpellHumanizer", true);
+            Menu.AddItem(new MenuItem("Enabled", "Enabled").SetValue(true));
+            Menu.AddItem(new MenuItem("Debug", "Debug").SetValue(false));
+            Game.OnGameUpdate += Game_OnGameUpdate;
+        }
+
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            LeagueSharp.Common.SpellHumanizer.Enabled = Menu.Item("Enabled").GetValue<bool>();
+            LeagueSharp.Common.SpellHumanizer.Debug = Menu.Item("Debug").GetValue<bool>();
         }
     }
 }
