80d444dfc43453e8cd18794a363714c783829eaa
trees-software <treesbot@gmail.com>
2014-11-25 13:51:28 -0500

    Add AppDomain Unloader

--- AttackDrawer/Program.cs
+++ AttackDrawer/Program.cs
@@ -2,10 +2,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
-using Color = System.Drawing.Color;
 
 #endregion
 
@@ -13,8 +13,8 @@ namespace AttackDrawer
 {
     internal class Program
     {
-
-        private static readonly Dictionary<Obj_AI_Hero, Render.Circle> CircleDictionary = new Dictionary<Obj_AI_Hero, Render.Circle>();
+        private static readonly Dictionary<Obj_AI_Hero, Render.Circle> CircleDictionary =
+            new Dictionary<Obj_AI_Hero, Render.Circle>();
 
         private static void Main(string[] args)
         {
@@ -23,24 +23,34 @@ namespace AttackDrawer
 
         private static void Game_OnGameLoad(EventArgs args)
         {
-            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValid && hero.IsEnemy))
+            foreach (Obj_AI_Hero hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValid && hero.IsEnemy))
             {
                 var circle = new Render.Circle(hero, Orbwalking.GetRealAutoAttackRange(hero), Color.Red, 4, true);
                 circle.Add();
                 CircleDictionary.Add(hero, circle);
             }
 
+            AppDomain.CurrentDomain.DomainUnload += CurrentDomain_DomainUnload;
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_DomainUnload;
             Game.OnGameUpdate += Game_OnGameUpdate;
             Game.PrintChat("Attack Range Loaded!");
+        }
+
+        private static void CurrentDomain_DomainUnload(object sender, EventArgs e)
+        {
+            foreach (var circle in CircleDictionary)
+            {
+                circle.Value.Dispose();
+            }
         }
 
         private static void Game_OnGameUpdate(EventArgs args)
         {
             foreach (var entry in CircleDictionary)
             {
-                var hero = entry.Key;
-                var circle = entry.Value;
-                var AARange = Orbwalking.GetRealAutoAttackRange(hero);
+                Obj_AI_Hero hero = entry.Key;
+                Render.Circle circle = entry.Value;
+                float AARange = Orbwalking.GetRealAutoAttackRange(hero);
 
                 circle.Radius = AARange;
                 circle.Color = ObjectManager.Player.Distance(hero) < AARange ? Color.Red : Color.LimeGreen;
