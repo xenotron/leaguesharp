ee6b9be545f80e684c60e6ec9ce957b816e875d8
trees-software <treesbot@gmail.com>
2014-11-30 05:18:31 -0500

    Add Circle

--- ControlSharp/Program.cs
+++ ControlSharp/Program.cs
@@ -4,6 +4,7 @@ using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
 using SharpDX.XInput;
+using Color = System.Drawing.Color;
 
 namespace ControlSharp
 {
@@ -14,10 +15,13 @@ namespace ControlSharp
         public static Menu Menu;
 
         public static Orbwalking.Orbwalker OrbWalker;
+        public static Orbwalking.OrbwalkingMode CurrentMode = Orbwalking.OrbwalkingMode.None;
+        public static Gamepad CurrentPad;
+        public static Render.Circle CurrentPosition;
 
         private static void Main(string[] args)
         {
-            Console.Clear();
+            //Console.Clear();
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
@@ -37,9 +41,60 @@ namespace ControlSharp
 
             Menu = new Menu("ControllerTest", "ControllerTest", true);
             OrbWalker = new Orbwalking.Orbwalker(Menu);
+            Menu.AddItem(new MenuItem("Draw", "Draw Circle").SetValue(true));
             Menu.AddToMainMenu();
 
+            if (Menu.Item("Draw").GetValue<bool>())
+            {
+                CurrentPosition = new Render.Circle(ObjectManager.Player.Position, 100, Color.Red, 2);
+                CurrentPosition.Add();
+            }
+
+            Menu.Item("Draw").ValueChanged += OnValueChanged;
             Game.OnGameUpdate += Game_OnGameUpdate;
+        }
+
+        private static void OnValueChanged(object sender, OnValueChangeEventArgs onValueChangeEventArgs)
+        {
+            if (onValueChangeEventArgs.GetNewValue<bool>())
+            {
+                CurrentPosition = new Render.Circle(ObjectManager.Player.Position, 100, Color.Red, 2);
+                CurrentPosition.Add();
+            }
+            else
+            {
+                CurrentPosition.Remove();
+            }
+        }
+
+        private static void UpdateStates()
+        {
+            CurrentPad = Control.GetState().Gamepad;
+            var b = CurrentPad.Buttons.ToString();
+
+            if (b == "None")
+            {
+                return;
+            }
+
+            if (b.Contains("DPadUp"))
+            {
+                CurrentMode = Orbwalking.OrbwalkingMode.Combo;
+            }
+            else if (b.Contains("DPadLeft"))
+            {
+                CurrentMode = Orbwalking.OrbwalkingMode.LaneClear;
+            }
+            else if (b.Contains("DPadRight"))
+            {
+                CurrentMode = Orbwalking.OrbwalkingMode.Mixed;
+            }
+            else if (!b.Contains("LeftThumb")) //Push any button to cancel mode
+            {
+                CurrentMode = Orbwalking.OrbwalkingMode.None;
+            }
+
+            OrbWalker.ActiveMode = CurrentMode;
         }
 
         private static void Game_OnGameUpdate(EventArgs args)
@@ -51,28 +106,17 @@ namespace ControlSharp
                 return;
             }
 
-            var state = Control.GetState().Gamepad;
-            var currentMode = Orbwalking.OrbwalkingMode.None;
-
-            if (state.LeftTrigger > 0)
-            {
-                currentMode = Orbwalking.OrbwalkingMode.Mixed;
-            }
-            if (state.RightTrigger > 0)
-            {
-                currentMode = Orbwalking.OrbwalkingMode.Combo;
-            }
-
-            OrbWalker.ActiveMode = currentMode;
+            UpdateStates();
             var pos = ObjectManager.Player.ServerPosition;
-            pos.X += state.LeftThumbX / 100f;
-            pos.Y += state.LeftThumbY / 100f;
-            
-            if (ObjectManager.Player.Distance(pos) < 50)
+            pos.X += CurrentPad.LeftThumbX / 100f;
+            pos.Y += CurrentPad.LeftThumbY / 100f;
+
+            if (ObjectManager.Player.Distance(pos) < 75)
             {
                 return;
             }
-            
+
+            CurrentPosition.Position = pos;
             OrbWalker.SetOrbwalkingPoint(pos);
         }
     }
