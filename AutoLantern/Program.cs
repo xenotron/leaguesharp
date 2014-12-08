95c211ddfe579e36f0ff102ca5bbd2da12962267
Trees <treesbot@gmail.com>
2014-12-08 12:49:31 -0500

    Fix
    Don't load when no thresh

--- AutoLantern/Program.cs
+++ AutoLantern/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
 
@@ -23,8 +24,10 @@ namespace AutoLantern
 
         private static void OnGameLoad(EventArgs args)
         {
-            if (ObjectManager.Player.ChampionName == "Thresh")
+            if (!ThreshInGame())
+            {
                 return;
+            }
 
             _menu = new Menu("AutoLantern", "AutoLantern", true);
             _menu.AddItem(new MenuItem("Auto", "Auto-Lantern at Low HP").SetValue(true));
@@ -42,20 +45,27 @@ namespace AutoLantern
 
         private static void OnGameUpdate(EventArgs args)
         {
-            if (IsValid(_threshLantern) && ((IsLow() && _menu.Item("Auto").GetValue<bool>()) || (_menu.Item("Hotkey").GetValue<KeyBind>().Active)))
+            if (IsValid(_threshLantern) &&
+                ((IsLow() && _menu.Item("Auto").GetValue<bool>()) || (_menu.Item("Hotkey").GetValue<KeyBind>().Active)))
+            {
                 InteractObject(_threshLantern);
+            }
         }
 
         private static void OnMinionCreation(GameObject obj, EventArgs args)
         {
             if (obj != null && obj.IsValid && obj.IsAlly && obj.Name.Contains(Lantern))
+            {
                 _threshLantern = obj;
+            }
         }
 
         private static void OnMinionDeletion(GameObject obj, EventArgs args)
         {
             if (obj != null && obj.IsValid && obj.IsAlly && obj.Name.Contains(Lantern))
+            {
                 _threshLantern = null;
+            }
         }
 
         private static bool IsLow()
@@ -68,13 +78,18 @@ namespace AutoLantern
             return lant != null && lant.IsValid && _player.ServerPosition.Distance(lant.Position) <= 500;
         }
 
+        private static bool ThreshInGame()
+        {
+            return ObjectManager.Get<Obj_AI_Hero>().Any(h => h.IsAlly && !h.IsMe && h.ChampionName == "Thresh");
+        }
+
         private static void InteractObject(GameObject obj)
         {
             using (var memoryStream = new MemoryStream())
             {
                 using (var binaryWriter = new BinaryWriter(memoryStream))
                 {
-                    binaryWriter.Write((byte)0x3A);
+                    binaryWriter.Write((byte) 0x3A);
                     binaryWriter.Write(_player.NetworkId);
                     binaryWriter.Write(obj.NetworkId);
                     Game.SendPacket(memoryStream.ToArray(), PacketChannel.C2S, PacketProtocolFlags.NoFlags);
