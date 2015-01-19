922a63ac167fdacb83e00fbb4c8b64e4b4492eb3
trees-software <treesbot@gmail.com>
2015-01-19 11:12:34 -0500

    temp bugsplat fix

--- LeBlanc/Clone.cs
+++ LeBlanc/Clone.cs
@@ -7,6 +7,7 @@ namespace LeBlanc
     internal class Clone
     {
         public static Menu LocalMenu;
+        public static Obj_AI_Base Pet;
 
         static Clone()
         {
@@ -23,6 +24,7 @@ namespace LeBlanc
             LocalMenu = clone;
 
             Game.OnGameUpdate += Game_OnGameUpdate;
+            GameObject.OnCreate += GameObject_OnCreate;
         }
 
         private static Menu Menu
@@ -45,44 +47,66 @@ namespace LeBlanc
             get { return ObjectManager.Player; }
         }
 
+        private static void GameObject_OnCreate(GameObject sender, EventArgs args)
+        {
+            if (sender == null || !sender.IsValid || !sender.Name.Equals(Player.Name))
+            {
+                return;
+            }
+
+            Pet = sender as Obj_AI_Base;
+        }
+
         private static void Game_OnGameUpdate(EventArgs args)
         {
-            if (!Enabled)
+            try
             {
-                return;
-            }
+                if (!Enabled)
+                {
+                    return;
+                }
 
-            var pet = Player.Pet as Obj_AI_Base;
-            var mode = Menu.Item("CloneMode").GetValue<StringList>().SelectedIndex;
+                var pet = Pet; //Player.Pet as Obj_AI_Base;
+                var mode = Menu.Item("CloneMode").GetValue<StringList>().SelectedIndex;
 
-            if (pet == null || !pet.IsValidPet())
-            {
-                return;
-            }
+                if (pet == null || !pet.IsValid) //!pet.IsValidPet()) //|| !pet.IsValidPet())
+                {
+                    return;
+                }
 
-            switch (mode)
-            {
-                case 0: // toward player
-                    var pos = Player.GetWaypoints().Count > 1 ? Player.GetWaypoints()[1].To3D() : Player.ServerPosition;
-                    Utility.DelayAction.Add(200, () => { pet.IssueOrder(GameObjectOrder.MovePet, pos); });
-                    break;
-                case 1: //toward target
-                    if (pet.CanAttack && !pet.IsWindingUp && !pet.Spellbook.IsAutoAttacking &&
-                        Target.IsValidTarget(Orbwalking.GetRealAutoAttackRange(pet)))
-                    {
+                if (mode == 1 &&
+                    !(pet.CanAttack && !pet.IsWindingUp && !pet.Spellbook.IsAutoAttacking &&
+                      Target.IsValidTarget(Orbwalking.GetRealAutoAttackRange(pet))))
+                {
+                    mode = 0;
+                }
+
+                switch (mode)
+                {
+                    case 0: // toward player
+                        var pos = Player.GetWaypoints().Count > 1
+                            ? Player.GetWaypoints()[1].To3D()
+                            : Player.ServerPosition;
+                        Utility.DelayAction.Add(200, () => { pet.IssueOrder(GameObjectOrder.MovePet, pos); });
+                        break;
+                    case 1: //toward target
                         pet.IssueOrder(GameObjectOrder.AutoAttackPet, Target);
-                    }
-                    break;
-                case 2: //away from player
-                    Utility.DelayAction.Add(
-                        100,
-                        () =>
-                        {
-                            pet.IssueOrder(
-                                GameObjectOrder.MovePet,
-                                (pet.Position + 500 * ((pet.Position - Player.Position).Normalized())));
-                        });
-                    break;
+                        break;
+                    case 2: //away from player
+                        Utility.DelayAction.Add(
+                            100,
+                            () =>
+                            {
+                                pet.IssueOrder(
+                                    GameObjectOrder.MovePet,
+                                    (pet.Position + 500 * ((pet.Position - Player.Position).Normalized())));
+                            });
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
     }
