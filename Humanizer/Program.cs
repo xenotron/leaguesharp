f6990770bc99699241d66d1cbea4f3b6b4086702
trees-software <treesbot@gmail.com>
2015-01-26 11:40:12 -0500

    added spell humanizing

--- Humanizer/Program.cs
+++ Humanizer/Program.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
 using LeagueSharp;
 using LeagueSharp.Common;
 
@@ -10,9 +11,11 @@ namespace Humanizer
 {
     public class Program
     {
-        public static Menu Config;
+        public static Menu Menu;
         public static float LastMove;
         public static Obj_AI_Base Player = ObjectManager.Player;
+        public static List<String> SpellList = new List<string> { "Q", "W", "E", "R" };
+        public static List<float> LastCast = new List<float>();
 
         private static void Main(string[] args)
         {
@@ -21,32 +24,62 @@ namespace Humanizer
 
         private static void Game_OnGameLoad(EventArgs args)
         {
-            Config = new Menu("Humanizer", "Humanizer", true);
-            Config.AddItem(new MenuItem("MovementDelay", "Movement Delay")).SetValue(new Slider(80, 0, 400));
-            Config.AddToMainMenu();
+            Menu = new Menu("Humanizer", "Humanizer", true);
+
+            var spells = Menu.AddSubMenu(new Menu("Spells", "Spells"));
+
+            for (var i = 0; i < 3; i++)
+            {
+                LastCast[i] = 0;
+                var spell = SpellList[i];
+                var menu = spells.AddSubMenu(new Menu(spell, spell));
+                menu.AddItem(new MenuItem("Enabled" + i, "Delay " + spell, true).SetValue(true));
+                menu.AddItem(new MenuItem("Delay" + i, "Cast Delay", true).SetValue(new Slider(80, 0, 400)));
+            }
+
+            var move = Menu.AddSubMenu(new Menu("Movement", "Movement"));
+            move.AddItem(new MenuItem("MovementEnabled", "Enabled").SetValue(true));
+            move.AddItem(new MenuItem("MovementDelay", "Movement Delay")).SetValue(new Slider(80, 0, 400));
+
+            Menu.AddToMainMenu();
 
             Obj_AI_Base.OnIssueOrder += Obj_AI_Base_OnIssueOrder;
+            Spellbook.OnCastSpell += Spellbook_OnCastSpell;
         }
 
-        private static void Obj_AI_Base_OnIssueOrder(Obj_AI_Base sender, GameObjectIssueOrderEventArgs args)
+        private static void Spellbook_OnCastSpell(Spellbook sender, SpellbookCastSpellEventArgs args)
         {
-            if (sender == null || !sender.IsValid || !sender.IsMe || args.Order != GameObjectOrder.MoveTo)
+            if (sender == null || !sender.Owner.IsMe || Menu.Item("Enabled" + (int) args.Slot).GetValue<bool>())
             {
                 return;
             }
 
-            if (Environment.TickCount - LastMove < GetDelay())
+            var delay = Menu.Item("Delay" + (int) args.Slot).GetValue<Slider>().Value;
+
+            if (Environment.TickCount - LastCast[(int) args.Slot] < delay)
+            {
+                args.Process = false;
+                return;
+            }
+
+            LastCast[(int) args.Slot] = Environment.TickCount;
+        }
+
+        private static void Obj_AI_Base_OnIssueOrder(Obj_AI_Base sender, GameObjectIssueOrderEventArgs args)
+        {
+            if (sender == null || !sender.IsValid || !sender.IsMe || args.Order != GameObjectOrder.MoveTo ||
+                !Menu.Item("MovementEnabled").GetValue<bool>())
+            {
+                return;
+            }
+
+            if (Environment.TickCount - LastMove < Menu.Item("MovementDelay").GetValue<Slider>().Value)
             {
                 args.Process = false;
                 return;
             }
 
             LastMove = Environment.TickCount;
-        }
-
-        private static float GetDelay()
-        {
-            return Config.Item("MovementDelay").GetValue<Slider>().Value;
         }
     }
 }
